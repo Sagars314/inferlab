637a6207e4cfa67500bd1dc5fb599af8239dddde
Sagar <sagars3.14@proton.me>
2026-03-09 11:46:41 +0530

    Initial inferlab package

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+#' Say Hello
+#'
+#' Prints a greeting message.
+#'
+#' @param name A character string.
+#' @return A greeting message
+#' @export
+hello_world <- function(name = "World") {
+  message(paste("Wccxc,", name, "!"))
+}
